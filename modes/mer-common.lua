2bada11ffbbd5e365d44ff775d8e1c259e82a16f
David Greaves <david@dgreaves.com>
2012-08-30 21:27:22 +0100

    Check if ~/.sbrules exists before including it
    Signed-off-by: David Greaves <david@dgreaves.com>

--- modes/mer-common.lua
+++ modes/mer-common.lua
@@ -3,6 +3,12 @@ function use_outside_path(path)
     {dir = path, rules = { {dir = path, use_orig_path = true}, }, })
 end
 
+-- http://stackoverflow.com/a/4991602/337649
+function file_readable(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
 -- Allow user-defined rules to "overlay" what we've defined here.
 -- What is in ~/.sbrules gets executed as if its contents were in this file, on this line.
 function run_sbrules()
@@ -14,7 +20,10 @@ function run_sbrules()
     if home then
         -- We could call .sbrules via pcall to trap errors but that just hides them from the user
         -- pcall(dofile, home .. '/.sbrules')
-        dofile(home .. '/.sbrules')
+	local sbrules = home .. '/.sbrules'
+	if file_readable(sbrules) then
+          dofile(sbrules)
+        end
     end
   end
 end
